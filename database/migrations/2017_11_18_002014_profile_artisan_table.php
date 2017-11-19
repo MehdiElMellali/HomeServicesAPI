a350f2d528b84eac9fa554677737280ebaec5f1e
youness iysa <youness.iysa@gmail.com>
2017-11-19 13:59:53 +0000

    add table artisan

---
+++ database/migrations/2017_11_18_002014_profile_artisan_table.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class ProfileArtisanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artisan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('adress');
+            $table->string('image');
+            $table->tinyInteger('phone')->unique();
+            $table->string('images');
+            $table->timestamp('dateentrer');
+            $table->integer('service_id')->unsigned();
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artisan');
+    }
+}
